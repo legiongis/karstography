12cc0f18ecc97b6d0c272182fa6d9b4b05bd9c48
adam cox <adam@legiongis.com>
2017-09-19 14:01:43 +0000

    add paths to sys.path to fix settings

--- karstography/wsgi.py
+++ karstography/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "karstography.settings")
+sys.path.append('/srv/karstography/karstography')
+sys.path.append('/srv/karstography')
 
 application = get_wsgi_application()
