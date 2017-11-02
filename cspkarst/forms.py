c8cc6a980a957a52ee5a8473abb2639f7219bc70
adam cox <mr.adamcox@gmail.com>
2017-11-02 13:00:24 -0500

    add sample help text
    Signed-off-by: adam cox <mr.adamcox@gmail.com>

--- cspkarst/forms.py
+++ cspkarst/forms.py
@@ -5,4 +5,6 @@ class SinkForm(ModelForm):
     class Meta:
         model = Sink
         fields = ['sink_type','dem_check','img_check','evidence','comment']
-        
+        help_texts = {
+            'sink_type': 'Do your best!',
+        }
