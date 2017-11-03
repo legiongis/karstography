a376566c89a624316aba8b1e70243acdb30d6cdd
adam cox <mr.adamcox@gmail.com>
2017-11-03 11:01:13 -0500

    add new fields to model
    Signed-off-by: adam cox <mr.adamcox@gmail.com>

--- cspkarst/models.py
+++ cspkarst/models.py
@@ -9,14 +9,23 @@ import os
 
 class Sink(models.Model):
 
-    CLASS_CHOICES = (
-        ("CATCHMENT","Catchment Basin"),
-        ("SINKHOLE","Sinkhole"),
-        ("OTHER","Other"),
+    TYPE_CHOICES = (
+       ("CATCHMENT","Catchment Basin"),
+       ("SINKHOLE","Sinkhole"),
+       ("QUARRY","Quarry"),
+       ("DC","Ditch/Culvert"),
+       ("FOUNDATION","Building Foundation"),
+       ("OTHER","Other"),
+       ("UNKNOWN","Unknown"),
+    )
+    
+    CON_CHOICES = (
+       ("PROBABLE","Probable"),
+       ("POSSIBLE","Possible"),
     )
 
     sink_id = models.IntegerField(null=True,blank=True)
-    sink_type = models.CharField(max_length=20,choices=CLASS_CHOICES,blank=True)
+    sink_type = models.CharField(max_length=20,choices=TYPE_CHOICES,blank=True)
     dem_check = models.IntegerField(null=True,blank=True)
     img_check = models.IntegerField(null=True,blank=True)
     evidence = models.CharField(max_length=1,blank=True,null=True)
@@ -24,6 +33,11 @@ class Sink(models.Model):
     elevation = models.FloatField(null=True,blank=True)
     in_nfhl = models.NullBooleanField()
     in_row = models.NullBooleanField()
+    bm_hs = models.BooleanField(default=False)
+    bm_aerial = models.BooleanField(default=False)
+    bm_tpi = models.BooleanField(default=False)
+    bm_usgs = models.BooleanField(default=False)
+    confidence = models.CharField(max_length=20,choices=CON_CHOICES,blank=True,null=True)
     comment = models.TextField(max_length=254,blank=True,null=True)
     geom = models.PointField(null=True)
     
