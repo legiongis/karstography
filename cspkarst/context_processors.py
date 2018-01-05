7f407beecc180b994a1d8d5855ba39f38c5b865b
adam cox <mr.adamcox@gmail.com>
2018-01-05 12:18:12 -0600

    add sinkhole counts to legend, reorder images

--- cspkarst/context_processors.py
+++ cspkarst/context_processors.py
@@ -1,8 +1,18 @@
 from django.conf import settings
+from cspkarst.models import Sink
     
 def context(request):
     return {
         'mapbox_api_key': settings.MAPBOX_API_KEY,
         'static_url': settings.STATIC_URL,
         'root_url': settings.ROOT_URL
+    }
+    
+def sink_counts(request):
+    sh_probable = len(Sink.objects.filter(sink_type="SINKHOLE",confidence="PROBABLE"))
+    sh_possible = len(Sink.objects.filter(sink_type="SINKHOLE",confidence="POSSIBLE"))
+    return {
+        'sinkhole_total': sh_probable+sh_possible,
+        'sinkhole_probable': sh_probable,
+        'sinkhole_possible': sh_possible
     }
--- karstography/settings.py
+++ karstography/settings.py
@@ -64,6 +64,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'cspkarst.context_processors.context',
+                'cspkarst.context_processors.sink_counts',
             ],
         },
     },
