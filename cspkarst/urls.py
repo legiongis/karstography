8eb1268208116faba85bda62c895e42710b5841d
adam cox <mr.adamcox@gmail.com>
2020-03-22 10:34:41 -0500

    remove unused djangorestframework
    Signed-off-by: adam cox <mr.adamcox@gmail.com>

--- cspkarst/urls.py
+++ cspkarst/urls.py
@@ -1,16 +1,11 @@
 from django.conf.urls import url, include
-from rest_framework.urlpatterns import format_suffix_patterns
-from rest_framework import routers
 from . import views
-
-router = routers.DefaultRouter()
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^json/(?P<sink_id>[\w-]+)/', views.sink_info, name='sink-info'),
     url(r'^sink-update/(?P<sink_id>[\w-]+)/', views.sink_update, name='sink-update'),
     url(r'^example-locations/', views.get_example_locations, name='example-locations'),
-    url(r'^', include(router.urls)),
     # url(r'^json/(?P<stairid>[\w-]+)/$', views.get_stairs, name='stair_json'),
 ]
 
