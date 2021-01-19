0c615b799f67e9aa4d796723382f1b9ec6b82656
Adam Cox <mr.adamcox@gmail.com>
2021-01-18 20:15:03 -0600

    remove leaflet admin

--- cspkarst/admin.py
+++ cspkarst/admin.py
@@ -2,12 +2,7 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from leaflet.admin import LeafletGeoAdmin
 from .models import Sink, TownUnit, Well, PointOfInterest
-
-class StairAdmin(LeafletGeoAdmin):
-    search_fields = ['type']
-    ordering = ('sink_id',)
 
 admin.site.register(Sink)
 admin.site.register(TownUnit)
