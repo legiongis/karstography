0392694216020d74ec84825c46f99fdf1e45ddcf
Adam Cox <mr.adamcox@gmail.com>
2022-12-08 00:16:08 -0600

    update django

--- karstography/urls.py
+++ karstography/urls.py
@@ -1,14 +1,11 @@
-from django.conf.urls import include, url
+from django.urls import path, include
 from django.contrib import admin
-from django.conf.urls.static import static
-from django.conf import settings
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    url(r'^login/$', auth_views.LoginView.as_view(), name='login'),
-    url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
-    url(r'^admin/', admin.site.urls),
-    url(r'^about/', include('docs.urls')),
-    url(r'', include('cspkarst.urls')),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('admin/', admin.site.urls),
+    path('about/', include('docs.urls')),
+    path('', include('cspkarst.urls')),
 ]
-# + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
