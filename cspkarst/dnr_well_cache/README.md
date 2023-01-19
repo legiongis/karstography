b787d9bb06412cfdc002888dc0f50ca110412edc
Adam Cox <mr.adamcox@gmail.com>
2023-01-19 17:49:25 -0600

    change cache dir location, put in settings

--- cspkarst/dnr_well_cache/README.md
+++
@@ -1,3 +0,0 @@
-### DNR Well Cache
-
-geojson data retrieved while loading wells from the DNR site is cached here.
--- karstography/settings.py
+++ karstography/settings.py
@@ -14,6 +14,9 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+CACHE_DIR = os.path.join(BASE_DIR, ".cache")
+if not os.path.isdir(CACHE_DIR):
+    os.mkdir(CACHE_DIR)
 
 DOCS_ROOT = os.path.join(BASE_DIR, 'docs/_build/html')
 
