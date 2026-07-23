b77b3a3baf4fdf56ed8dd69712f15e1ad6bf3a06
Adam Cox <mr.adamcox@gmail.com>
2026-07-22 23:04:45 -0500

    upgrade deps to support svelte 5

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,9 @@
 import '../node_modules/ol/ol.css';
 import './css/global.css';
+import { mount } from 'svelte';
 import App from './App.svelte';
 
-const app = new App({
+const app = mount(App, {
 	target: document.body,
 	props: JSON.parse(document.getElementById("svelteprops").textContent)
 });
