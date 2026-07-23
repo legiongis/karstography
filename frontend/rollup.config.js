7b8de7748c8a5648aa027e89de0ef8318ff482c5
Adam Cox <mr.adamcox@gmail.com>
2026-07-22 23:01:09 -0500

    remove serve() to fix error with resolve() after svelte 5

--- frontend/rollup.config.js
+++ frontend/rollup.config.js
@@ -1,40 +1,19 @@
 import svelte from 'rollup-plugin-svelte';
-import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
 import terser from '@rollup/plugin-terser';
 import css from 'rollup-plugin-css-only';
 
 const production = !process.env.ROLLUP_WATCH;
 
-function serve() {
-	let server;
-
-	function toExit() {
-		if (server) server.kill(0);
-	}
-
-	return {
-		writeBundle() {
-			if (server) return;
-			server = require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
-				stdio: ['ignore', 'inherit', 'inherit'],
-				shell: true
-			});
-
-			process.on('SIGTERM', toExit);
-			process.on('exit', toExit);
-		}
-	};
-}
-
 export default {
 	input: 'src/main.js',
 	output: {
-		sourcemap: true,
+		file: '../cspkarst/static/frontend/bundle.js',
 		format: 'iife',
+		sourcemap: true,
 		name: 'app',
-		file: '../cspkarst/static/frontend/bundle.js'
 	},
 	plugins: [
 		svelte({
@@ -57,10 +36,6 @@ export default {
 			dedupe: ['svelte']
 		}),
 		commonjs(),
-
-		// In dev mode, call `npm run start` once
-		// the bundle has been generated
-		!production && serve(),
 
 		// Watch the `public` directory and refresh the
 		// browser on changes when not in production
